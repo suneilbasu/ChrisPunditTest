838d79e4736be093ca2ab6ca21f0e2eb340bf71c
Suneil Basu <suneil@futureprogress.com>
2016-02-24 22:34:24 +0000

    removed the name field from the user table which was nil and causing problems

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221153521) do
+ActiveRecord::Schema.define(version: 20160224223304) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20160221153521) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
     t.integer  "role"
   end
 
