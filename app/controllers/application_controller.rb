4e0ea4fd7247e1ef3435ff359c9090996cf117fe
09croberts <09c.roberts@dsh-pms.com>
2016-02-24 10:10:47 +0000

    Updated readme for link to tutorial followed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   private
   	def user_not_authorized
   		flash[:alert] = "Access Denied."
-  		redirect_to(request.referrer) || root_path
+  		redirect_to(request.referrer) || 'users#index'
   	end
 end
